bad438158f89d96582fd68b6abbcfb94b3e89235
c0a24290 <c0a24290c3@edu.teu.ac.jp>
2025-04-22 14:52:22 +0900

    練習3:はみ出し判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -6,12 +6,30 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
-    pg.K_UP: (0,-5),
-    pg.K_DOWN: (0,+5),
-    pg.K_LEFT: (-5,0),
-    pg.K_RIGHT: (+5,0),
-    }
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """ 
+    引数：こうかとんRectまたは爆弾Rect
+    戻り値：判定結果タプル（横，縦）
+    画面内ならTrue,画面外ならFalse
+    """
+    yoko, tate = True, True  # 横，縦方向用の変数
+    # 横方向判定
+    if rct.left < 0 or WIDTH < rct.right:  # 画面外だったら
+        yoko = False
+    # 縦方向判定
+    if rct.top < 0 or HEIGHT < rct.bottom: # 画面外だったら
+        tate = False
+    return yoko, tate
+
+
 
 
 def main():
@@ -25,9 +43,10 @@ def main():
     # 爆弾初期化
     bb_img = pg.Surface((20, 20))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
-    bb_img.set_colorkey((0, 0, 0))
     bb_rct = bb_img.get_rect()
-    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT)
+    bb_img.set_colorkey((0, 0, 0))
     vx, vy = +5, +5
 
     clock = pg.time.Clock()
@@ -40,11 +59,11 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
+
         for key, mv in DELTA.items():
             if key_lst[key]:
                 sum_mv[0] += mv[0]  # 左右方向
                 sum_mv[1] += mv[1]  # 上下方向
-        
         # if key_lst[pg.K_UP]:
         #     sum_mv[1] -= 5
         # if key_lst[pg.K_DOWN]:
@@ -54,8 +73,15 @@ def main():
         # if key_lst[pg.K_RIGHT]:
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True): # 画面外だったら
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1]) # 画面内に戻す
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx, vy)  # 爆弾の移動
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:  # 左右どちらかにはみ出ていたら
+            vx *= -1
+        if not tate:  # 上下どちらかにはみ出ていたら
+            vy *= -1
         screen.blit(bb_img, bb_rct)  # 爆弾の描画
         pg.display.update()
         tmr += 1
